08b4430d3da1ce9d7682927d5bce77ea650bb399
vrk <vrkwork@gmail.com>
2017-05-15 12:48:26 -0700

    minor

--- express-routes/server.js
+++ express-routes/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Main page!');
+  res.send('Main page');
 });
 
 app.get('/hello', function (req, res) {
